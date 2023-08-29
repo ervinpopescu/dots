56d75e6b294ebe35dfbdeb801b92c8ee3771c378
ervinpopescu <ervin.popescu10@gmail.com>
2023-08-29 16:37:16 +0300

    added lazy.nvim config

--- configs/.config/nvim/lua/config/autocmds.lua
+++ configs/.config/nvim/lua/config/autocmds.lua
@@ -1,9 +1,27 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 Idgroup = vim.api.nvim_create_augroup("MyGroup", { clear = true })
-vim.api.nvim_clear_autocmds { group = "MyGroup" }
+vim.api.nvim_clear_autocmds({ group = "MyGroup" })
 vim.api.nvim_create_autocmd("InsertEnter", {
   pattern = { "*" },
   command = "normal zz",
+  group = Idgroup,
+})
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = { "*" },
+  command = "set guicursor=a:ver20",
+  group = Idgroup,
+})
+-- vim.api.nvim_create_autocmd("VimLeave", {
+--   pattern = { "*" },
+--   command = 'printf "e[6 q"',
+--   group = Idgroup,
+-- })
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = { "*" },
+  command = "set guicursor=a:ver20",
   group = Idgroup,
 })
 vim.api.nvim_create_autocmd("VimLeave", {
@@ -36,11 +54,11 @@ vim.api.nvim_create_autocmd("VimLeave", {
   command = "!clear",
   group = Idgroup,
 })
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  command = "ColorizerAttachToBuffer",
-  group = Idgroup,
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = "*",
+--   command = "ColorizerAttachToBuffer",
+--   group = Idgroup,
+-- })
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = { "*" },
   -- command = "set wrap list listchars=space:· linebreak"
@@ -52,8 +70,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
   command = "set nowrap nolist nolinebreak",
   group = Idgroup,
 })
-vim.cmd [[
+vim.cmd([[
 augroup filetypedetect
   au! BufRead,BufNewFile *.m set filetype=octave
 augroup END
-]]
+]])
--- configs/.config/nvim/lua/plugins/core.lua
+++ configs/.config/nvim/lua/plugins/core.lua
@@ -3,18 +3,22 @@ return {
     "folke/todo-comments.nvim",
     event = "User AstroFile",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function() require("todo-comments").setup {} end,
+    config = function()
+      require("todo-comments").setup({})
+    end,
   },
   {
     "Pocco81/auto-save.nvim",
-    config = function() require("auto-save").setup { debounce_delay = 1000 } end,
+    config = function()
+      require("auto-save").setup({ debounce_delay = 1000 })
+    end,
     lazy = false,
   },
   {
     "ethanholz/nvim-lastplace",
     event = "BufRead",
     config = function()
-      require("nvim-lastplace").setup {
+      require("nvim-lastplace").setup({
         lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
         lastplace_ignore_filetype = {
           "gitcommit",
@@ -23,13 +27,15 @@ return {
           "hgcommit",
         },
         lastplace_open_folds = true,
-      }
+      })
     end,
   },
   {
     "iamcco/markdown-preview.nvim",
     ft = "markdown",
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
     config = function()
       vim.g.mkdp_auto_start = 1
       vim.g.mkdp_auto_close = 0
@@ -45,7 +51,7 @@ return {
   {
     "cappyzawa/trim.nvim",
     config = function()
-      require("trim").setup {
+      require("trim").setup({
         -- if you want to ignore markdown file.
         -- you can specify filetypes.
         ft_blocklist = { "markdown" },
@@ -55,13 +61,15 @@ return {
           [[%s/\($\n\s*\)\+\%$//]],
           [[%s/\(\n\n\)\n\+/\1/]],
         },
-      }
+      })
     end,
     lazy = false,
   },
   {
     "folke/zen-mode.nvim",
-    config = function() require("zen-mode").setup {} end,
+    config = function()
+      require("zen-mode").setup({})
+    end,
     lazy = false,
   },
   {
@@ -73,7 +81,7 @@ return {
       vim.g.vimtex_compiler_silent = true
       vim.g.vimtex_quickfix_mode = 0
       vim.g.tex_conceal = "abdmg"
-      vim.cmd [[
+      vim.cmd([[
       let g:vimtex_syntax_conceal = {
           \ 'accents': 1,
           \ 'ligatures': 1,
@@ -89,16 +97,16 @@ return {
           \ 'sections': 1,
           \ 'styles': 1,
           \}
-      ]]
-      vim.cmd "set conceallevel=2"
-      vim.cmd 'let maplocalleader = ","'
+      ]])
+      vim.cmd("set conceallevel=2")
+      vim.cmd('let maplocalleader = ","')
     end,
     lazy = false,
   },
   {
     "L3MON4D3/LuaSnip",
     config = function()
-      require("luasnip.loaders.from_lua").load { paths = "~/.config/nvim/LuaSnip/" }
+      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/LuaSnip/" })
       require("luasnip.loaders.from_vscode").lazy_load()
     end,
     lazy = false,
@@ -107,21 +115,21 @@ return {
   {
     "sirver/ultisnips",
     config = function()
-      vim.cmd [[
+      vim.cmd([[
         let g:UltiSnipsExpandTrigger = '<tab>'
         let g:UltiSnipsJumpForwardTrigger = '<tab>'
         let g:UltiSnipsJumpBackwardTrigger = '<s-tab>'
         let g:UltiSnipsSnippetDirectories=[$HOME.'/.config/nvim/UltiSnips']
-      ]]
+      ]])
     end,
     lazy = false,
   },
   {
     "KeitaNakamura/tex-conceal.vim",
     config = function()
-      vim.cmd [[
+      vim.cmd([[
         hi Conceal ctermbg=none
-      ]]
+      ]])
     end,
     lazy = false,
   },
@@ -132,7 +140,7 @@ return {
   {
     "stevearc/oil.nvim",
     config = function()
-      require("oil").setup {
+      require("oil").setup({
         columns = {
           "icon",
           "permissions",
@@ -140,8 +148,15 @@ return {
           "mtime",
         },
         view_options = { show_hidden = true },
-      }
+      })
     end,
   },
   { "mg979/vim-visual-multi" },
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require("symbols-outline").setup()
+    end,
+    lazy = false,
+  },
 }
