07f3ee88974702db8811f5aa10f847b2adf8f153
ervinpopescu <ervin.popescu10@gmail.com>
2026-04-07 10:22:16 +0300

    qtile: add color-based drawing to BatteryIcon widget
    Override draw() to tint the icon based on charge state (red/orange/yellow/green).
    Remove usemask and hardcoded fontsize defaults; switch defaults to a list.

--- dot_config/qtile-wl/extras/widgets/battery_icon.py
+++ dot_config/qtile-wl/extras/widgets/battery_icon.py
@@ -1,3 +1,4 @@
+from libqtile.utils import rgb
 from libqtile.widget.battery import BatteryState, BatteryStatus
 from qtile_extras import widget
 
@@ -5,15 +6,35 @@ from modules.settings import colors
 
 
 class BatteryIcon(widget.BatteryIcon):
-    defaults = (
+    defaults = [
         ("update_interval", 60, "Update interval in seconds"),
-        ("fontsize", 34, ""),
         ("foreground", colors["fg2"], ""),
-    )
+    ]
 
     def __init__(self, **config):
         widget.BatteryIcon.__init__(self, **config)
         self.add_defaults(BatteryIcon.defaults)
+
+    def _get_color(self) -> str:
+        icon = self.current_icon
+        if "caution" in icon:
+            return colors["red"]
+        if "low" in icon:
+            return colors["orange"]
+        if "charged" in icon or "full" in icon:
+            return colors["lightgreen"]
+        return colors["yellow"]
+
+    def draw(self) -> None:
+        self.drawer.clear(self.background or self.bar.background)
+        image = self.images[self.current_icon]
+        self.drawer.ctx.save()
+        self.drawer.ctx.translate(self.padding, (self.bar.size - image.height) // 2)
+        r, g, b, a = rgb(self._get_color())
+        self.drawer.ctx.set_source_rgba(r, g, b, a)
+        self.drawer.ctx.mask(image.pattern)
+        self.drawer.ctx.restore()
+        self.draw_at_default_position()
 
     @staticmethod
     def _get_icon_key(status: BatteryStatus) -> str:
