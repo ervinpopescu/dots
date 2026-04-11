0ea159bba43320165b8888da278a05e20ef74760
ervinpopescu <ervin.popescu10@gmail.com>
2026-04-11 03:58:24 +0300

    feat(rofi): parameterize UI scaling and fix hardcoded path

--- dot_config/rofi/powermenu.sh
+++ dot_config/rofi/powermenu.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
 rofi -show power-menu \
-     -modi "power-menu:/home/ervin/.local/bin/rofi-power-menu --choices=suspend/logout/reboot/shutdown --confirm=reboot/shutdown" \
-     -config /home/ervin/.config/rofi/powermenu.rasi
+     -modi "power-menu:$HOME/.local/bin/rofi-power-menu --choices=suspend/logout/reboot/shutdown --confirm=reboot/shutdown" \
+     -config $HOME/.config/rofi/powermenu.rasi
