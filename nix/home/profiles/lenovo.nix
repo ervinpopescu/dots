c7a1751f7dbc91c032e84140628cda3c62bb82cd
ervinpopescu <ervin.popescu10@gmail.com>
2026-08-20 20:40:11 +0300

    feat(nix): add Lenovo Transmission service

--- nix/home/profiles/lenovo.nix
+++ nix/home/profiles/lenovo.nix
@@ -1,4 +1,45 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
+let
+  secretFile = ../../../secrets/lenovo.yaml;
+  hasSecret = builtins.pathExists secretFile;
+  transmissionPackage = lib.attrByPath [ "transmission_4" ] (lib.attrByPath [ "transmission" ] null pkgs) pkgs;
+  hasTransmission = transmissionPackage != null;
+  transmissionSource = builtins.readFile ../../data/transmission-settings.json;
+  transmissionSettings = lib.replaceStrings
+    [ "__TRANSMISSION_RPC_PASSWORD__" ]
+    [ config.sops.placeholder."transmission/rpc-password" ]
+    transmissionSource;
+in
 {
   imports = [ ./desktop.nix ];
+
+  home.packages = lib.optional hasTransmission transmissionPackage;
+
+  sops = lib.optionalAttrs hasSecret {
+    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+    secrets."transmission/rpc-password" = {
+      sopsFile = secretFile;
+    };
+    templates."transmission-settings.json".content = transmissionSettings;
+  };
+
+  home.activation.transmissionSettings = lib.mkIf hasSecret (
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      install -Dm600 ${config.sops.templates."transmission-settings.json".path} \
+        "$HOME/.config/transmission-daemon/settings.json"
+  ''
+  );
+
+  systemd.user.services.transmission = lib.mkIf (hasSecret && hasTransmission) {
+    Unit = {
+      Description = "Transmission daemon";
+      After = [ "network-online.target" ];
+    };
+    Service = {
+      ExecStart = "${transmissionPackage}/bin/transmission-daemon --foreground --config-dir=%h/.config/transmission-daemon";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
 }
